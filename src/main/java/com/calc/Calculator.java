f696fd3234dae2aa127de87f0456d6703c9e1564
Maksym <maxmailbox777@gmail.com>
2021-09-25 12:30:25 +0300

    Initial commit

---
+++ src/main/java/com/calc/Calculator.java
@@ -0,0 +1,20 @@
+package com.calc;
+
+public class Calculator {
+
+    public double getSum(double firstValue, double secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public double getSubtraction(double firstValue, double secondValue) {
+        return firstValue - secondValue;
+    }
+
+    public double getMultiplication(double firstValue, double secondValue) {
+        return firstValue * secondValue;
+    }
+
+    public double getDivide(double firstValue, double secondValue) {
+        return firstValue / secondValue;
+    }
+}
---
+++ src/main/java/com/calc/Main.java
@@ -0,0 +1,53 @@
+package com.calc;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        startCalculator();
+    }
+
+    public static void startCalculator() throws NumberFormatException{
+        String welcomeMassage = "Welcome to Console Calculator for Base Camp\n" +
+                "Please press \"Enter\" after each typed number or operation\n";
+        String firstNumberMessage = "Enter first number";
+        String secondNumberMessage = "Enter second number";
+        String operationMessage = "Select operation between +,-,*,/";
+        String errorMessage = "Unsupported operation";
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        Calculator calculator = new Calculator();
+
+        System.out.println(welcomeMassage);
+
+        while (true) {
+            System.out.println(firstNumberMessage);
+            double firstNumber = scanner.nextDouble();
+            System.out.println(operationMessage);
+            String operation = scanner.next();
+            System.out.println(secondNumberMessage);
+            double secondNumber = scanner.nextDouble();
+
+            switch (operation) {
+                case "+":
+                    System.out.println(calculator.getSum(firstNumber, secondNumber));
+                    break;
+                case "-":
+                    System.out.println(calculator.getSubtraction(firstNumber, secondNumber));
+                    break;
+                case "*":
+                    System.out.println(calculator.getMultiplication(firstNumber, secondNumber));
+                    break;
+                case "/":
+                    System.out.println(calculator.getDivide(firstNumber, secondNumber));
+                    break;
+                default:
+                    System.out.println(errorMessage);
+            }
+
+        }
+    }
+}
